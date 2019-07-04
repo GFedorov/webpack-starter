711b146eaf2c8a7917d8ec08ff49b4b2f29ec046
Gri_Fedorov <fedorov.grigory@gmail.com>
2019-07-04 10:48:09 +0300

    function pick up dubina added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,51 +1,101 @@
 import '../styles/index.scss';
-function interSect(obj1,obj2,destination) {
- var deltaX = Math.abs(obj1.x-obj2.x);
- var deltaY = Math.abs(obj1.y-obj2.y);
- return (deltaX**2 + deltaY**2)<destination**2;
+
+function interSect(obj1, obj2, destination) {
+    var deltaX = Math.abs(obj1.x - obj2.x);
+    var deltaY = Math.abs(obj1.y - obj2.y);
+    return (Math.pow(deltaX, 2) + Math.pow(deltaY, 2)) < Math.pow(destination, 2);
 }
+
 function createMonster(stageEl) {
     var x = 400;
     var y = 200;
-    var monster = new Monster('idblob',stageEl,x,y);
+    var monster = new Monster('blob', stageEl, x, y);
+    var monster2 = new Monster('blob2', stageEl, 700, y);
+
     return monster;
 }
-function Monster(id, stageEl, x, y, speed) {
-  var me = this;
-  var el = document.createElement('div');
-  el.setAttribute('id', id);
-  el.setAttribute('class', 'monster');
-  stageEl.appendChild(el);
-  this.x = x;
-  this.y = y;
-  this.speed = speed || 10;
-  // this.moveToObject = function(targetObject) {
-  //   var deltaX = targetObject.x - me.x;
-  //   var deltaY = targetObject.y - me.y;
-  //   if (deltaX < 0) {
-  //     me.x -= me.speed;
-  //   } else {
-  //     me.x += me.speed;
-  //   }
-  //   if (deltaY < 0) {
-  //     me.y -= me.speed;
-  //   } else {
-  //     me.y += me.speed;
-  //   }
-  // }
-  this.destroy = function() {
-    el.classList.add("fired");
-    setTimeout(function() {
-      stageEl.removeChild(el);
-    }, 1000);
-  };
-  this.draw = function() {
 
-    el.style.left = this.x  + 'px';
-    el.style.top = this.y  + 'px';
-  };
-  this.draw();
+function Monster(id, stageEl, x, y, speed) {
+    var me = this;
+    var el = document.createElement('div');
+    el.setAttribute('id', id);
+    el.setAttribute('class', 'monster');
+    stageEl.appendChild(el);
+    this.x = x;
+    this.y = y;
+    this.speed = speed || 10;
+    // this.moveToObject = function(targetObject) {
+    //   var deltaX = targetObject.x - me.x;
+    //   var deltaY = targetObject.y - me.y;
+    //   if (deltaX < 0) {
+    //     me.x -= me.speed;
+    //   } else {
+    //     me.x += me.speed;
+    //   }
+    //   if (deltaY < 0) {
+    //     me.y -= me.speed;
+    //   } else {
+    //     me.y += me.speed;
+    //   }
+    // }
+    this.destroy = function() {
+        el.classList.add("fired");
+        setTimeout(function() {
+            stageEl.removeChild(el);
+        }, 1000);
+    };
+    this.draw = function() {
+
+        el.style.left = this.x + 'px';
+        el.style.top = this.y + 'px';
+    };
+    this.draw();
 }
+
+function createWeapon(stageEl) {
+    var x = 100;
+    var y = 200;
+    var weapon = new Weapon('weapon', stageEl, x, y);
+    return weapon;
+}
+
+function Weapon(id, stageEl, x, y, speed) {
+    var me = this;
+    var el = document.createElement('div');
+    el.setAttribute('id', id);
+    el.setAttribute('class', 'dubina');
+    stageEl.appendChild(el);
+    this.x = x;
+    this.y = y;
+    this.speed = speed || 10;
+    // this.moveToObject = function(targetObject) {
+    //   var deltaX = targetObject.x - me.x;
+    //   var deltaY = targetObject.y - me.y;
+    //   if (deltaX < 0) {
+    //     me.x -= me.speed;
+    //   } else {
+    //     me.x += me.speed;
+    //   }
+    //   if (deltaY < 0) {
+    //     me.y -= me.speed;
+    //   } else {
+    //     me.y += me.speed;
+    //   }
+    // }
+    this.destroy = function() {
+        el.setAttribute("class", "fired");
+        setTimeout(function() {
+            stageEl.removeChild(el);
+        }, 1000);
+    };
+    this.draw = function() {
+
+        el.style.left = this.x + 'px';
+        el.style.top = this.y + 'px';
+    };
+    this.draw();
+}
+
 //main script
 
 window.addEventListener('load', function() {
@@ -62,18 +112,21 @@ window.addEventListener('load', function() {
     sword.setAttribute('class', 'sword');
     mainHero.appendChild(sword);
 
+
     stage.appendChild(mainHero);
 
     var gameWorld = document.getElementById('gameworld');
-    var hitRound = document.querySelector('.hit-area'); 
+    var hitRound = document.querySelector('.hit-area');
     var backgroundPosX = 0;
     var backgroundPosY = 0;
     var deltaPosX = 0;
     var deltaPosY = 0;
     var speedKoeff = 0.1;
 
-    console.log({centerX, centerY});
-   var monster =  createMonster(gameWorld);
+    console.log({ centerX, centerY });
+    var monster = createMonster(gameWorld);
+    var weapon = createWeapon(gameWorld);
+    
 
     stage.onmousemove = function(event) {
         deltaPosX = (event.clientX - stageCoords.x - centerX) * speedKoeff;
@@ -86,24 +139,32 @@ window.addEventListener('load', function() {
         angle = angle - 90;
         mainHero.style.transform = 'translate(-50%,-50%) rotate(' + angle + 'deg)';
     };
-    stage.onclick=function(){
+    stage.onclick = function() {
         mainHero.classList.add('fight');
-        if(interSect({x:-backgroundPosX + centerX, y:-backgroundPosY + centerY},monster,70)){
+        if (interSect({ x: -backgroundPosX + centerX, y: -backgroundPosY + centerY }, monster, 70)) {
             monster.destroy();
         }
-        setTimeout(function(){
+        if (interSect({ x: -backgroundPosX + centerX, y: -backgroundPosY + centerY }, weapon, 70)) {
+            var dubina = document.createElement('div');
+            dubina.setAttribute('id', 'dubina');
+            dubina.setAttribute('class', 'dubina');
+            mainHero.removeChild(sword);
+            mainHero.appendChild(dubina);
+            weapon.destroy();
+        }
+        setTimeout(function() {
             mainHero.classList.remove('fight');
-        },300);
+        }, 300);
     };
 
     console.log(centerX);
     setInterval(function() {
         backgroundPosX = backgroundPosX - Math.round(deltaPosX);
         backgroundPosY = backgroundPosY - Math.round(deltaPosY);
-        
+
         hitRound.style.left = -backgroundPosX + centerX + 'px';
         hitRound.style.top = -backgroundPosY + centerY + 'px';
-        gameWorld.style.transform = 'translate('+ backgroundPosX + 'px, '+ backgroundPosY + 'px)' ;
-        
+        gameWorld.style.transform = 'translate(' + backgroundPosX + 'px, ' + backgroundPosY + 'px)';
+
     }, 50);
 });
